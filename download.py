d4878b37dae33fb4f97841485553c19c270c8ab6
frozenbanana97 <kylem0119@gmail.com>
2022-04-25 14:31:21 -0400

    data selection and download complete

--- download.py
+++ download.py
@@ -5,22 +5,54 @@ from shapely import wkt
 import os.path
 from os import mkdir
 from os import listdir
+from datetime import date, datetime
 
+def location():
+   wktlocation = input('input WKT string')
+   print('The given location is: ' + wktlocation)
+   return wktlocation
 
-#perform the search for results with the given location and date range, use if centroid yields no results
+def startDate():
+    dateStart = input('input start date in YYYY-MM-DD')
+    format = '%Y-%m-%d'
+    startRes = True
+    endRes = True
+    try:
+        startRes = bool(datetime.strptime(dateStart, format))
+    except ValueError:
+        startRes = False
+        
+    print('Start Date: ' + dateStart)
+    print("Does start date match format? : " + str(startRes))
+    return dateStart
 
-def geo_loc(location, dateStart, dateEnd):
+def endDate():
+    dateEnd = input('input end date in YYYY-MM-DD')
+    format = '%Y-%m-%d'
+    startRes = True
+    endRes = True
+    try:
+        endRes = bool(datetime.strptime(dateEnd, format))
+    except ValueError:
+        endRes = False
+    
+    print('End Date: ' + dateEnd)
+    print("Does end date match format? : " + str(endRes))    
+    return dateEnd
+
+def geo_loc(wktlocation, dateStart, dateEnd):
     geo_results = asf.geo_search(
-        intersectsWith=location,
+        intersectsWith=wktlocation,
         platform=asf.PLATFORM.SENTINEL1,
         maxResults=10,
         start=dateStart,
         end=dateEnd)
 
     print(f'{len(geo_results)} results found')
+    return geo_results
 
-def centroid_loc(location, dateStart, dateEnd):
-    centroidwkt = wkt.loads(location)
+def centroid_loc(wktlocation, dateStart, dateEnd):
+    centroidwkt = wkt.loads(wktlocation)
     centroid = centroidwkt.centroid.wkt
 
     centroid_results = asf.geo_search(
@@ -31,11 +63,13 @@ def centroid_loc(location, dateStart, dateEnd):
         end=dateEnd)
 
     print(f'{len(centroid_results)} results found')
+    return centroid_results
 
-def scene_download(scene_list):
+def scene_loc(scene_list):
     granule_results = asf.granule_search(scene_list)
 
     print(f'{len(granule_results)} results found')
+    return granule_results
 
 def dir_create():
     #set the downlaod directory, or create it if it doesnt exist
@@ -58,8 +92,10 @@ def ASF_login():
         print(f'Auth failed: {e}')
     else:
         print('Success!')
+    return user_pass_session
 
-def download_products(results, user_pass_session):
-    results.download(path='./downloads', session=user_pass_session)
+def download_products(results, user_session):
+    results.download(path='./downloads', session=user_session)
 
     listdir('./downloads')
+
